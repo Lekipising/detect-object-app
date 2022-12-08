dd19381aa346c6af3a14fb2f52a7b51b3e78598f
Liplan Lekipising <leeplanke@gmail.com>
2022-12-09 00:44:30 +0200

    add logic and integrate API

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import axios from "axios";
 import Head from "next/head";
 import React, { useState } from "react";
@@ -6,15 +7,31 @@ export default function Home() {
   const [videoFile, setVideoFile] = useState(null);
   const [loading, setLoading] = useState(false);
 
+  const [objName, setObjName] = useState("");
+
+  const [noClassFound, setNoClassFound] = useState(false);
+
+  const [base64, setBase64] = useState(null);
+
   const handleSubmit = async (e) => {
     setLoading(true);
     e.preventDefault();
+    const obj = {
+      json: objName.toLowerCase(),
+    };
+    const json = JSON.stringify(obj);
+    const blob = new Blob([json], {
+      type: "application/json",
+    });
     const formData = new FormData();
     formData.append("file", videoFile);
+    formData.append("json", blob);
 
     // axios config
     const res = await axios.post(
-      `https://object-detect.onrender.com/detect`,
+      process.env.NODE_ENV === "development"
+        ? `http://127.0.0.1:5000/detect`
+        : `https://object-detect.onrender.com/detect`,
       formData,
       {
         headers: {
@@ -24,10 +41,22 @@ export default function Home() {
       }
     );
     setLoading(false);
-    console.log(res);
+    if (res.data.base64 === "No match found") {
+      setNoClassFound(true);
+      return;
+    }
+    setBase64(res.data.base64);
   };
 
-  const [showInputField, setShowInputField] = useState(true);
+  const countBoolsTrue = (arr) => {
+    let count = 0;
+    arr.forEach((item) => {
+      if (item) {
+        count++;
+      }
+    });
+    return count;
+  };
 
   return (
     <>
@@ -38,40 +67,32 @@ export default function Home() {
           content="width=device-width, initial-scale=1 maximum-scale=1 user-scalable=no"
         />
       </Head>
-      <main className="min-h-screen w-screen flex justify-center items-center text-white flex-col gap-32">
-        <div className="flex flex-col gap-4">
-          <h1 className="text-2xl font-bold">Welcome to the game! </h1>
-          <h2 className="">Instructions are simple:</h2>
-          <ul className="ml-8 list-decimal">
-            <li>Upload a video</li>
-            <li>Wait for our wizard app to process</li>
-            <li>Type an object you saw on the video</li>
-            <li>Know if got it correct</li>
-            <li>Good luck!</li>
-          </ul>
-        </div>
-        <form className="flex flex-col gap-8">
-          {showInputField && (
+      <main className="min-h-screen w-screen flex justify-center items-center text-white gap-32">
+        <div className="flex flex-col gap-8">
+          <div className="flex flex-col gap-4 bg-purple-600/20 p-8 rounded-lg">
+            <h1 className="text-2xl font-bold">Welcome to the game! </h1>
+            <h2 className="">Instructions are simple:</h2>
+            <ul className="ml-8 list-decimal">
+              <li>Upload a video</li>
+              <li>Type an object in the video</li>
+              <li>Process the video</li>
+              <li>Get results</li>
+              <li>Good luck!</li>
+            </ul>
+          </div>
+          <form className="flex flex-col gap-8 bg-purple-600/20 p-8 rounded-lg">
+            {/* step one */}
+
             <>
               <input
                 type="file"
                 accept="video/*"
                 onChange={(e) => setVideoFile(e.target.files[0])}
               />
-              <button
-                type="submit"
-                disabled={loading || !videoFile}
-                className="bg-blue-700 text-lg font-semibold px-8 py-1 rounded-lg"
-                onClick={(e) => handleSubmit(e)}
-              >
-                {loading ? "Loading..." : "Process Video"}
-              </button>
-            </>
-          )}
-          {!showInputField && (
-            <>
               <input
                 type="text"
+                value={objName}
+                onChange={(e) => setObjName(e.target.value)}
                 placeholder="Enter type of object..."
                 className="font-semibold px-8 text-sm py-2 rounded-lg text-[#686868]"
                 id="object"
@@ -79,14 +100,49 @@ export default function Home() {
               />
               <button
                 type="submit"
-                className="bg-white rounded-lg px-8 py-2 text-blue-700 font-bold"
-                onClick={(e) => console.log(e)}
+                disabled={loading || !videoFile || !objName}
+                className="bg-blue-700 text-lg font-semibold px-8 py-1 rounded-lg"
+                onClick={(e) => handleSubmit(e)}
               >
-                {loading ? "Loading..." : "Check Answer"}
+                {loading ? "Loading..." : "Process Video"}
               </button>
             </>
+          </form>
+        </div>
+
+        <div
+          className={`flex transition-all ease-in duration-300 flex-col gap-8 ${
+            countBoolsTrue([videoFile, base64]) * 96
+          }px}`}
+        >
+          {videoFile && (
+            <div className="flex flex-col gap-4">
+              <h2 className="font-bold text-lg">Uploaded Video</h2>
+              <video
+                src={URL.createObjectURL(videoFile)}
+                controls
+                className="w-96 h-auto"
+              />
+            </div>
           )}
-        </form>
+          {base64 && (
+            <div className="flex flex-col gap-4">
+              <h2 className="font-bold text-lg text-green-500">
+                Detected object
+              </h2>
+              <img
+                src={`data:image/png;base64,${base64}`}
+                alt="video"
+                className="w-96 h-auto"
+              />
+            </div>
+          )}
+          {noClassFound && (
+            <h2 className="font-bold text-lg text-red">
+              We did not find that object in the video, try again!
+            </h2>
+          )}
+        </div>
       </main>
     </>
   );
